a2dcb3100baf0bb37b7aa41658b4e939c23ea7bb
Eric <erictarn@gmail.com>
2013-05-16 00:08:05 -0400

    new characters

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-c = Character.create(:hanzi => "媽", :zhuyin_fuhao => "ㄇㄚ", :pinyin => "ma", :english => "Mom")
-c.audio = File.open("#{ Rails.root }/public/ma.m4a")
-c.save
-
-c = Character.create(:hanzi => "爸", :zhuyin_fuhao => "ㄅㄚˋ", :pinyin => "ba", :english => "Dad")
-c.audio = File.open("#{ Rails.root }/public/ba.m4a")
-c.save
-
-c = Character.create(:hanzi => "你", :zhuyin_fuhao => "ㄋㄧˇ", :pinyin => "ni", :english => "You")
+c = Character.create(:hanzi => "你", :zhuyin_fuhao => "", :pinyin => "ni", :english => "You")
 c.audio = File.open("#{ Rails.root }/public/ni.m4a")
 c.save
 
-c = Character.create(:hanzi => "十", :zhuyin_fuhao => "ㄕˊ", :pinyin => "shi", :english => "Ten")
-c.audio = File.open("#{ Rails.root }/public/shi.m4a")
+c = Character.create(:hanzi => "您", :zhuyin_fuhao => "", :pinyin => "nin", :english => "You (Polite)")
+c.audio = File.open("#{ Rails.root }/public/nin.m4a")
+c.save
+
+c = Character.create(:hanzi => "好", :zhuyin_fuhao => "", :pinyin => "hao", :english => "Good")
+c.audio = File.open("#{ Rails.root }/public/hao.m4a")
+c.save
+
+c = Character.create(:hanzi => "請", :zhuyin_fuhao => "", :pinyin => "qing", :english => "Please")
+c.audio = File.open("#{ Rails.root }/public/qing.m4a")
+c.save
+
+c = Character.create(:hanzi => "問", :zhuyin_fuhao => "", :pinyin => "wen", :english => "Ask")
+c.audio = File.open("#{ Rails.root }/public/wen.m4a")
+c.save
+
+c = Character.create(:hanzi => "貴", :zhuyin_fuhao => "", :pinyin => "gui", :english => "Expensive")
+c.audio = File.open("#{ Rails.root }/public/gui.m4a")
+c.save
+
+c = Character.create(:hanzi => "姓", :zhuyin_fuhao => "", :pinyin => "xing", :english => "Surname")
+c.audio = File.open("#{ Rails.root }/public/xing.m4a")
 c.save
